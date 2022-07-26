78cb7c3da2fd99b88f8ffe969e17acc89be25921
Alexander <Sakeran@gmail.com>
2022-07-26 14:33:25 -0400

    Use UI color for settings 'apply' button

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -4,9 +4,17 @@
 </script>
 
 <button
-  class:bg-ui-red={!disabled}
-  class:bg-ui-dull-red={disabled}
-  class="font-sans rounded-pill text-white px-12 py-4 enabled:hover:bg-ui-light-red enabled:focus:bg-ui-light-red motion-safe:transition-colors motion-safe:duration-500"
+  class="font-sans rounded-pill text-white px-12 py-4 motion-safe:transition-colors motion-safe:duration-500"
   on:click
   {disabled}>{text}</button
 >
+
+<style lang="postcss">
+  button {
+    background-color: theme("colors.gray");
+  }
+
+  button:enabled {
+    background-color: var(--ui-color);
+  }
+</style>
